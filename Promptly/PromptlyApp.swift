6362d68cf2fda728cd708347fb9d9332fb496dcf
bhavishnadar <bhavish.nadar@intercom.io>
2025-08-20 20:52:36 +0100

    Initial Commit

---
+++ Promptly/PromptlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  PromptlyApp.swift
+//  Promptly
+//
+//  Created by Bhavish Nader on 20/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct PromptlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PromptlyTests/PromptlyTests.swift
@@ -0,0 +1,16 @@
+//
+//  PromptlyTests.swift
+//  PromptlyTests
+//
+//  Created by Bhavish Nader on 20/08/2025.
+//
+
+import Testing
+
+struct PromptlyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
